6cf04fcf18e6f93edd8a8c779a8ece3831689ce5
Mel Gray <melgray@gmail.com>
2016-07-20 14:13:23 -0700

    Checking in last bit of work I did on this.  Thanks for the memories

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Stellar Motel is a library written in C used for parsing h264 and AAC streams ou
 
 Supported network protocols:
   * RTP
+
+
+About
+-----
+
+This isn't finished or polished, but it can work with a bit of cajoling.
+
+This started as a sideproject.  A different approach to parsing streaming video files on mobile devices.
+
+APIs became available in iOS9 that negated the need for this file based approach.
+
+This project could still be useful on Linux and other platforms, but until I have an itch that needs scratching on those platforms, I will probably not be updating this.
--- examples/streaming_read.c
+++ examples/streaming_read.c
@@ -18,9 +18,46 @@ int main(int argc, char **argv) {
   }
 
   __block int done = 0;
+  char cursor[4];
+  char *c = cursor;
 
   mp4_streaming_reader(argv[1], ^(char *data, ssize_t size) {
+
     printf("%ld\n", size);
+
+    for (size_t i = 0; i < size; i++) {
+
+      c[0] = c[1];
+      c[1] = c[2];
+      c[2] = c[3];
+      c[3] = data[i];
+
+      if (chunkIsAtom(c)) {
+        printf("GOT ATOM! %s\n", c);
+
+        // MP4Atom_t *atom;
+        // atom = (MP4Atom_t *)malloc(sizeof(MP4Atom_t));
+        //
+        // atomPtr[4] = 0;
+        // int startPos =
+        //     fpPos - 8; // 4 bytes for identifier and 4 bytes for length
+        // fseek(fp, startPos, SEEK_SET);
+        //
+        // unsigned char q[4];
+        // fread(&q, sizeof(char), 4, fp);
+        // int length = to_host(q);
+        // fseek(fp, startPos + 8, SEEK_SET);
+        //
+        // strcpy(atom->type, atomPtr);
+        // atom->position = startPos;
+        // atom->length = length;
+        //
+        // atom->child = NULL;
+        // atom->sibling = NULL;
+        // atom->parent = NULL;
+      }
+    }
+
   });
 
   while (done != 1) {
